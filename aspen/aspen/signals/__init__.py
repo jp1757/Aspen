8ed24953d210c739e4e2eb72dd79246b2d7e7534
jp1757 <james.m.r.peter@gmail.com>
2024-06-13 21:47:12 -0400

    Adding signal combination & normalisation structure

--- aspen/aspen/signals/__init__.py
+++ aspen/aspen/signals/__init__.py
@@ -2,6 +2,6 @@
 Signals package that sets out a structure for defining signal objects
 """
 
-from aspen.signals.core import ISignal
+from aspen.signals.core import ISignal, ISignals, INormalise
 from aspen.signals.leaf import ILeaf, Leaf, LeafSeries
 from aspen.signals.generic import Signal
--- aspen/aspen/signals/core.py
+++ aspen/aspen/signals/core.py
@@ -9,9 +9,42 @@ import pandas as pd
 
 class ISignal(metaclass=abc.ABCMeta):
     """
-    Core Signal Interface
+    Core Individual Signal Interface
     """
 
     @abc.abstractmethod
     def calculate(self) -> pd.DataFrame:
+        """
+        Calculate a signal object & return data
+        :return: pd.DataFrame
+        """
         pass
+
+
+class ISignals(metaclass=abc.ABCMeta):
+    """
+    Sets out a structure for normalising & then combining
+    multiple individual signal (ISignal) objects.
+    """
+
+    @abc.abstractmethod
+    def combine(self, normalise: bool) -> pd.DataFrame:
+        """
+        Combine multiple signals
+        :param normalise: (bool) whether to normalise signal values before combining
+        :return: pd.DataFrame
+        """
+        pass
+
+
+class INormalise(metaclass=abc.ABCMeta):
+    """
+    Sets out structure of signal normalisation objects
+    """
+
+    @abc.abstractmethod
+    def norm(self) -> pd.DataFrame:
+        """
+        Normalise signal data
+        :return: (pd.DataFrame) normalised signal data
+        """
