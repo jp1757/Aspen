edb77fc214d54db68f10ee274699f5b1564a973f
jp1757 <james.m.r.peter@gmail.com>
2024-06-14 15:21:29 -0400

    Working on btest

--- aspen/aspen/backtest/generic.py
+++ aspen/aspen/backtest/generic.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from aspen.backtest.core import IBTest
 from aspen.signals.core import ISignal
+from aspen.pcr.core import IPortConstruct
 
 
 class BTest(IBTest):
@@ -17,8 +18,10 @@ class BTest(IBTest):
     def __init__(
             self,
             *,
+            dates: pd.DatetimeIndex,
             tr: pd.DataFrame,
             signals: List[ISignal],
+            pcr: IPortConstruct,
             sdate: pd.Timestamp = pd.Timestamp(year=2000, month=1, day=1),
             edate: pd.Timestamp = pd.Timestamp.now().date(),
     ):
