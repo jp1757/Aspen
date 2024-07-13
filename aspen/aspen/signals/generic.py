74b398670d8de94b09c6be68560c9889cff2f176
jp1757 <james.m.r.peter@gmail.com>
2024-07-13 09:28:43 -0400

    Base signals object

--- aspen/aspen/signals/generic.py
+++ aspen/aspen/signals/generic.py
@@ -5,7 +5,8 @@ from typing import Dict
 
 import pandas as pd
 
-from aspen.signals import ISignal, ILeaf
+from aspen.signals.core import ISignal, ISignals
+from aspen.signals.leaf import ILeaf
 
 
 class Signal(ISignal):
@@ -33,3 +34,39 @@ class Signal(ISignal):
             self.data[leaf.key] = signal
 
         return signal
+
+
+class Signals(ISignals):
+    """
+    Stores multiple signal objects but does *not* combine them.
+    Access them by passing the signal name to build function
+    """
+
+    def __init__(self, *signals: ISignal):
+
+        self._signals = {s.name: s for s in signals}
+        if len(signals) > len(self._signals):
+            raise ValueError(
+                f"Duplicate signal name suspected: {[s.name for s in signals]}"
+            )
+
+    def _combine(self) -> pd.DataFrame:
+        """Override this function with specific signal combination logic"""
+        raise ValueError(
+            "This implementation does combine signals. Please set the name "
+            "parameter to retrieve specific signal data"
+        )
+
+    def build(self, name: str = None) -> pd.DataFrame:
+        """
+        Serve up signal data by setting the 'name' parameter
+
+        :param name: (str, optional) name of signal to return
+        :return: pd.DataFrame of signal data indexed by date with columns set
+            to asset ids
+        """
+
+        if name is None:
+            return self._combine()
+        else:
+            return self._signals[name].calculate()
