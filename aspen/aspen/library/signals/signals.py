13ce431354117d0428baf3ffc2462d5235ea1ab2
jp1757 <james.m.r.peter@gmail.com>
2024-11-11 11:18:25 -0500

    Updated cross-sectional mean implementation

--- aspen/aspen/library/signals/signals.py
+++ aspen/aspen/library/signals/signals.py
@@ -1,5 +1,5 @@
 """
-SignalHeap interface implementations
+Signals object implementations
 """
 
 import pandas as pd
@@ -7,17 +7,36 @@ import pandas as pd
 from aspen.signals.generic import Signals
 
 
-class SMean(Signals):
-    """
-    Combine multiple signal objects by taking a cross-sectional mean
-    across assets on each date
-    """
+class XSMean(Signals):
+    """Calculate the cross-sectional mean value of all signals on each date"""
+
+    def __init__(self, *args, dropna: str = "any", **kwargs):
+        """
+        Init XSMean object
+        :param args: *args to pass to base class Signals
+        :param dropna: (str) drop dates if there are 'any' NaN values or 'all' NaNs
+        :param kwargs: **kwargs to pass to base class Signals
+        """
+        self.dropna = dropna
+        super().__init__(*args, **kwargs)
 
     def _combine(self) -> pd.DataFrame:
-        """Combination logic"""
-        df = pd.concat([s.calculate() for k, s in self._signals.items()])
-        df = df.stack().groupby(level=[0, 1]).mean().unstack().sort_index()
+        """Calculate mean across all signals on each date"""
 
-        assert not df.index.duplicated().any(), "Duplicate index values found"
+        # Calculate each signal & combine into one dataframe incorporating a multi-index
+        # made up of date & asset id, with one column for each signal value
+        df = (
+            pd.concat(
+                [pd.Series(x.calculate().stack(), name=x.name) for x in self.signals],
+                axis=1,
+            )
+            .unstack()
+            .ffill()
+            .dropna(how=self.dropna)
+            .stack()
+        )
 
-        return df
+        # Take a cross-sectional mean across each asset for each date
+        sig_mean = df.mean(axis=1).unstack()
+
+        return sig_mean
